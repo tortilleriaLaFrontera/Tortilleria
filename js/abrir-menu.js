112bd5b91cd4138f891681362cb2ef689d8feb58
Indeed3269 <rcoronadaniel@gmail.com>
2025-02-27 12:25:40 -0800

    cerrado de menu con interacciones externas, mas info en nosotros html

--- js/abrir-menu.js
+++ js/abrir-menu.js
@@ -3,3 +3,13 @@ function abrirmenu() {
     enlaces.classList.toggle('active');
     
 }
+
+//para cerrar el menu si hago click en otra parte
+document.addEventListener('click', function (event) {
+    const menu = document.querySelector('.links');
+    const btnMenu = document.querySelector('.menu');
+
+    if (!menu.contains(event.target) && !btnMenu.contains(event.target)) {
+        menu.classList.remove('active');
+    }
+})
