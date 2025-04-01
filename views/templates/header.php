d98248ca7237ee8d524c7d6a432df5b814cbc688
Indeed3269 <rcoronadaniel@gmail.com>
2025-04-01 00:14:57 -0700

    name change

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Login MVC</title>
+    <title>Tortilleria la Frontera</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/main.css">
     
